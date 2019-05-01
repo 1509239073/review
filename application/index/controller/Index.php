a9505c402effc31a2477a2e7d9f67023dd5ae781
allen.tai <1509239073@qq.com>
2019-05-01 12:05:14 +0800

    init

---
+++ application/index/controller/Index.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+// use app\index\model\User as UserModel;
+use think\facade\Config;
+use think\facade\Cache;
+use think\facade\Session;
+use think\facade\Request;
+
+class Index
+{
+
+    public function index()
+    {
+        echo phpinfo();
+    }
+
+    public function million_data_test()
+    {
+        //$usersList = Db::name('test')->field('id,name,time')->select();
+        //dump($usersList);
+        Db::transaction(function () {
+            $str = '';
+            for ($i = 0; $i < 1000000; $i++) {
+                $tmp = random_self(6);
+                $str .= "('" . $tmp . "','" . date('Y-m-d H:i:s', time()) . "'),";
+
+
+            }
+            //$insertSql = 'insert into test (name,time) values'.rtrim($str,',');
+            //Db::execute($insertSql);
+            //$usersList = Db::name('test')->field('id,name,time')->select();
+            // dump($usersList);
+        });
+        // 
+        // echo 2;
+
+
+    }
+}
---
+++ application/index/controller/Notes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taixing
+ * Date: 2019/4/30
+ * Time: 16:55
+ */
+
+namespace app\index\controller;
+
+/**
+ * Class Notes
+ * @package app\index\controller
+ */
+class Notes
+{
+
+
+    public function  aas($id){
+
+    }
+}
